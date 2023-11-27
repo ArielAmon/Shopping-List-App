ec7d4603e63877b84d6d37604781543f6f3572d6
Ariel Amon <arielamon1445@gmail.com>
2023-11-27 09:14:31 +0200

    Defne main componnets

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
 })
-export class RecipesComponent {
-
+export class RecipesComponent implements OnInit {
+  ngOnInit(): void {}
 }
